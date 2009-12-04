eb4fb9ab7569279317f507c4655e97dbf133c6e5
Rafael Rosa Fu <rafaelrosafu@rafaelrosafu.(none)>
2009-12-04 01:08:42 -0200

    A pilha tem uma lista de caixas

--- lib/pile.rb
+++ lib/pile.rb
@@ -1,4 +1,10 @@
 class Pile
+  attr_reader :boxes
+
+  def initialize(*boxes)
+    @boxes = boxes
+  end
+
   def height
     0
   end
--- spec/pile_spec.rb
+++ spec/pile_spec.rb
@@ -5,5 +5,10 @@ describe Pile do
     pile = Pile.new
     pile.height.should == 0
   end
+
+  it "should receive a list of boxes" do
+    pile = Pile.new(1,2,3)
+    pile.boxes.should == [1,2,3]
+  end
 end
 
