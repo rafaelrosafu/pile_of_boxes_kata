e4f51c4fc9df14408b14dcd76e9adf588fe1a5da
Rafael Rosa Fu <rafaelrosafu@rafaelrosafu.(none)>
2009-12-04 01:06:47 -0200

    A pilha tem uma altura

--- lib/pile.rb
+++ lib/pile.rb
@@ -1,3 +1,6 @@
 class Pile
+  def height
+    0
+  end
 end
 
--- spec/pile_spec.rb
+++ spec/pile_spec.rb
@@ -1,5 +1,9 @@
 require 'lib/pile.rb'
 
 describe Pile do
+  it "should have a height" do
+    pile = Pile.new
+    pile.height.should == 0
+  end
 end
 
